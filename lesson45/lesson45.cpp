35cdbc228753b085d01b769f70d16607a18878a4
Артур Валентей <valentejvioletta@gmail.com>
2024-03-04 18:41:20 +0300

    вывели все результаты функций

--- lesson45/lesson45.cpp
+++ lesson45/lesson45.cpp
@@ -5,7 +5,9 @@ using namespace std;
 int sum_of_negative_elements(int* vector, int size);
 void random_init(int* vector, int size, int a, int b);
 string convert(int* vector, int size);
-
+int proizvidenie_of_max_and_min_elements(int* vector, int size);
+int index_of_min_elements(int* vector, int size);
+int index_of_max_elements(int* vector, int size);
 
 int main()
 {
@@ -21,7 +23,8 @@ int main()
 
 	random_init(vector, size, -10, 10);
 	cout << convert(vector, size) << endl;
-	cout << "sum of negative: "<<  sum_of_negative_elements(vector, size);
+	cout << "sum of negative: " << sum_of_negative_elements(vector, size) << endl;
+	cout << "proizvedenie: " << proizvidenie_of_max_and_min_elements(vector, size) << endl;
 	delete[] vector;
 	return 0;
 }
