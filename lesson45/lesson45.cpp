eedbb011425add376ac1dd1c128143d291792be7
Артур Валентей <valentejvioletta@gmail.com>
2024-03-04 17:25:07 +0300

    добавили 2 функции конертации и рандом инит

--- lesson45/lesson45.cpp
+++ lesson45/lesson45.cpp
@@ -3,6 +3,14 @@
 using namespace std;
 int main()
 {
-  
+	int size;
+	
+	do 
+	{
+		cout << "input size: ";
+		cin >> size;
+	} while (size <= 0);
+
+	return 0;
 }
 
---
+++ lesson45/util1.cpp
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string>
+#include <iostream>
+using namespace std;
+
+void random_init(int* vector, int size, int a,int b) {
+	for (int i = 0; i < size; i++) {
+		*(vector + i) = rand() % (b - a + 1) + a;
+	}
+}
+
+
+string convert(int* vector, int size) {
+	string s = "";
+	
+	for (int i = 0; i < size; i++) {
+		s += to_string(*(vector + i)) + " ";
+	}
+	
+	return s;
+}
